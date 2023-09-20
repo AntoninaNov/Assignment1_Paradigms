3ca47e608f79ebf58f007475dd439304c57223dd
AntNov <tonianovak@yahoo.com>
2023-09-20 11:09:03 +0300

    Procedural style, fixed search and file saving

--- main.c
+++ main.c
@@ -8,12 +8,15 @@ typedef struct TextNode {
     struct TextNode *next;
 } TextNode;
 
-void displayMenuAndClearConsole() {
+void clearConsole() {
 #ifdef _WIN32
     system("cls");
 #else
     system("clear");
 #endif
+}
+
+void displayMenu() {
     printf("Console cleared\n");
     printf("Please select an option from the menu below:\n");
     printf(" 1 - Append at the end\n");
@@ -21,14 +24,14 @@ void displayMenuAndClearConsole() {
     printf(" 3 - Save to file\n");
     printf(" 4 - Load from file\n");
     printf(" 5 - Print to console\n");
-    printf(" 6 - Insert text by indexes\n");
+    printf(" 6 - Insert text by line and index\n");
     printf(" 7 - Search\n");
-    printf(" 8 - Clear console\n");
+    printf(" 8 - Clear console and display menu\n");
     printf(" 9 - Exit\n");
     printf("Your choice: ");
 }
 
-TextNode *createTextNode(int contentLength) {
+TextNode *createTextNode(int contentLength) { // allocating memory for a TextNode and for the content of that node
     TextNode *newNode = (TextNode *)malloc(sizeof(TextNode));
     newNode->content = (char *)calloc(contentLength, sizeof(char));
     newNode->next = NULL;
@@ -66,13 +69,170 @@ void freeTextNodeList(TextNode *startNode) {
     }
 }
 
+void appendToEnd(TextNode *listHead, int bufferLength) {
+    printf("Append to the end: ");
+    char textToAppend[bufferLength];
+    fgets(textToAppend, sizeof(textToAppend), stdin);
+    size_t textLength = strlen(textToAppend);
+    if (textLength > 0 && textToAppend[textLength - 1] == '\n') {
+        textToAppend[textLength - 1] = '\0';
+    }
+
+    TextNode *lastTextNode = findLastTextNode(listHead);
+    if(strlen(lastTextNode->content) == 0) {
+        strcpy(lastTextNode->content, textToAppend);
+    } else {
+        TextNode *newNode = createTextNode(bufferLength);
+        strcpy(newNode->content, textToAppend);
+        lastTextNode->next = newNode;
+    }
+}
+
+void startNewLine(TextNode *listHead, int bufferLength) {
+    printf("New line started\n");
+    TextNode *currentTextNode = createTextNode(bufferLength);
+    findLastTextNode(listHead)->next = currentTextNode;
+}
+
+void saveToFile(TextNode *listHead, int bufferLength) {
+    char filename[bufferLength];
+    printf("Enter the file name for saving: ");
+    fgets(filename, bufferLength, stdin);
+    strtok(filename, "\n");  // remove newline
+
+    FILE* file = fopen(filename, "w");
+    if (file) {
+        TextNode *currentNode = listHead;
+        while (currentNode) {
+            fputs(currentNode->content, file);
+            if (currentNode->next) {
+                fputc('\n', file);
+            }
+            currentNode = currentNode->next;
+        }
+        fclose(file);
+        printf("Text has been saved successfully\n");
+    } else {
+        printf("Error: Could not save to file!\n");
+    }
+}
+
+void loadFromFile(TextNode *listHead, int bufferLength) {
+    char filename[bufferLength];
+    printf("Enter the file name for loading: ");
+    fgets(filename, bufferLength, stdin);
+    strtok(filename, "\n");  // remove newline
+
+    FILE* file = fopen(filename, "r");
+    if (file) {
+        // Clearing out the existing linked list
+        freeTextNodeList(listHead->next);
+        listHead->next = NULL;
+
+        TextNode *currentNode = listHead;
+        char fileLineContent[bufferLength];
+        bool isFirstLine = true;
+
+        while (fgets(fileLineContent, sizeof(fileLineContent), file)) {
+            size_t textLength = strlen(fileLineContent);
+            if (textLength > 0 && fileLineContent[textLength - 1] == '\n') {
+                fileLineContent[textLength - 1] = '\0'; // Remove the newline character
+            }
+
+            if(isFirstLine) {
+                isFirstLine = false;
+            } else {
+                currentNode->next = createTextNode(bufferLength);
+                currentNode = currentNode->next;
+            }
+
+            free(currentNode->content);
+
+            // Copy the content from the file line into the node's content
+            currentNode->content = strdup(fileLineContent);
+        }
+
+        fclose(file);
+        printf("Text has been loaded successfully\n");
+    } else {
+        printf("Error: Could not load from file!\n");
+    }
+}
+
+void printToConsole(TextNode *listHead) {
+    TextNode *currentNode = listHead;
+    while (currentNode) {
+        printf("%s\n", currentNode->content);
+        currentNode = currentNode->next;
+    }
+}
+
+void insertTextByIndexes(TextNode *listHead, int bufferLength) {
+    int lineIndex, charIndex;
+    printf("Enter line and index (e.g., 0 6): ");
+    scanf("%d %d", &lineIndex, &charIndex);
+    while ((getchar()) != '\n');
+
+    char insertText[bufferLength];
+    printf("Enter text to insert: ");
+    fgets(insertText, sizeof(insertText), stdin);
+    size_t textLength = strlen(insertText);
+    if (textLength > 0 && insertText[textLength - 1] == '\n') {
+        insertText[textLength - 1] = '\0';
+    }
+
+    TextNode *targetNode = findTextNodeAtIndex(listHead, lineIndex);
+    if (!targetNode || charIndex > strlen(targetNode->content) || charIndex < 0) {
+        printf("Invalid index provided!\n");
+        return;
+    }
+
+    char updatedContent[bufferLength*2];
+    memset(updatedContent, 0, sizeof(updatedContent));
+    strncpy(updatedContent, targetNode->content, charIndex);
+    strcat(updatedContent, insertText);
+    strcat(updatedContent, &targetNode->content[charIndex]);
+
+    free(targetNode->content);
+    targetNode->content = strdup(updatedContent);
+}
+
+void searchTextInList(TextNode *listHead, int bufferLength) {
+    char searchText[bufferLength];
+    printf("Enter text to search: ");
+    fgets(searchText, sizeof(searchText), stdin);
+    size_t textLength = strlen(searchText);
+    if (textLength > 0 && searchText[textLength - 1] == '\n') {
+        searchText[textLength - 1] = '\0';
+    }
+
+    TextNode *currentNode = listHead;
+    bool isFound = false;
+    int lineNumber = 0;
+    while (currentNode) {
+        char *startOfContent = currentNode->content;
+        char *foundPos = strstr(startOfContent, searchText);
+
+        while (foundPos) {
+            printf("Found on line %d at position %ld: %s\n", lineNumber, (long)(foundPos - startOfContent), currentNode->content);
+            isFound = true;
+            startOfContent = foundPos + textLength;
+            foundPos = strstr(startOfContent, searchText);
+        }
+
+        lineNumber++;
+        currentNode = currentNode->next;
+    }
+    if (!isFound) {
+        printf("Text not found!\n");
+    }
+}
+
 int main() {
     int bufferLength = 256;  // Set to a reasonable default size
-
     TextNode *listHead = createTextNode(bufferLength);
-    TextNode *currentTextNode = listHead;
 
-    displayMenuAndClearConsole();
+    displayMenu();
 
     char userCommand;
     while (true) {
@@ -82,155 +242,29 @@ int main() {
 
         switch (userCommand) {
             case '1':
-            {
-                printf("Append to the end: ");
-                char textToAppend[bufferLength];
-                fgets(textToAppend, sizeof(textToAppend), stdin);
-                size_t textLength = strlen(textToAppend);
-                if (textLength > 0 && textToAppend[textLength - 1] == '\n') {
-                    textToAppend[textLength - 1] = '\0';
-                }
-
-                TextNode *lastTextNode = findLastTextNode(listHead);
-                if(strlen(lastTextNode->content) == 0) {
-                    strcpy(lastTextNode->content, textToAppend);
-                } else {
-                    TextNode *newNode = createTextNode(bufferLength);
-                    strcpy(newNode->content, textToAppend);
-                    lastTextNode->next = newNode;
-                }
+                appendToEnd(listHead, bufferLength);
                 break;
-            }
             case '2':
-                printf("New line started\n");
-                currentTextNode = createTextNode(bufferLength);
-                findLastTextNode(listHead)->next = currentTextNode;
+                startNewLine(listHead, bufferLength);
                 break;
             case '3':
-            {
-                char filename[bufferLength];
-                printf("Enter the file name for saving: ");
-                fgets(filename, bufferLength, stdin);
-                strtok(filename, "\n");  // remove newline
-
-                FILE* file = fopen(filename, "w");
-                if (file) {
-                    TextNode *currentNode = listHead;
-                    while (currentNode) {
-                        fputs(currentNode->content, file);
-                        if (currentNode->next) {
-                            fputc('\n', file);
-                        }
-                        currentNode = currentNode->next;
-                    }
-                    fclose(file);
-                    printf("Text has been saved successfully\n");
-                } else {
-                    printf("Error: Could not save to file!\n");
-                }
+                saveToFile(listHead, bufferLength);
                 break;
-            }
             case '4':
-            {
-                char filename[bufferLength];
-                printf("Enter the file name for loading: ");
-                fgets(filename, bufferLength, stdin);
-                strtok(filename, "\n");  // remove newline
-
-                FILE* file = fopen(filename, "r");
-                if (file) {
-                    TextNode *currentNode = listHead;
-                    char fileLineContent[bufferLength];
-                    while (fgets(fileLineContent, sizeof(fileLineContent), file)) {
-                        size_t textLength = strlen(fileLineContent);
-                        if (textLength > 0 && fileLineContent[textLength - 1] == '\n') {
-                            fileLineContent[textLength - 1] = '\0';
-                        }
-
-                        if (!currentNode) {
-                            currentNode = createTextNode(bufferLength);
-                            findLastTextNode(listHead)->next = currentNode;
-                        }
-
-                        free(currentNode->content);
-                        currentNode->content = strdup(fileLineContent);
-                        currentNode = currentNode->next;
-                    }
-                    fclose(file);
-                    printf("Text has been loaded successfully\n");
-                } else {
-                    printf("Error: Could not load from file!\n");
-                }
+                loadFromFile(listHead, bufferLength);
                 break;
-            }
             case '5':
-            {
-                TextNode *currentNode = listHead;
-                while (currentNode) {
-                    printf("%s\n", currentNode->content);
-                    currentNode = currentNode->next;
-                }
+                printToConsole(listHead);
                 break;
-            }
             case '6':
-            {
-                int lineIndex, charIndex;
-                printf("Enter line and index (e.g., 0 6): ");
-                scanf("%d %d", &lineIndex, &charIndex);
-                while ((getchar()) != '\n');
-
-                char insertText[bufferLength];
-                printf("Enter text to insert: ");
-                fgets(insertText, sizeof(insertText), stdin);
-                size_t textLength = strlen(insertText);
-                if (textLength > 0 && insertText[textLength - 1] == '\n') {
-                    insertText[textLength - 1] = '\0';
-                }
-
-                TextNode *targetNode = findTextNodeAtIndex(listHead, lineIndex);
-                if (!targetNode || charIndex > strlen(targetNode->content) || charIndex < 0) {
-                    printf("Invalid index provided!\n");
-                    break;
-                }
-
-                char updatedContent[bufferLength*2];
-                memset(updatedContent, 0, sizeof(updatedContent));
-                strncpy(updatedContent, targetNode->content, charIndex);
-                strcat(updatedContent, insertText);
-                strcat(updatedContent, &targetNode->content[charIndex]);
-
-                free(targetNode->content);
-                targetNode->content = strdup(updatedContent);
+                insertTextByIndexes(listHead, bufferLength);
                 break;
-            }
             case '7':
-            {
-                char searchText[bufferLength];
-                printf("Enter text to search: ");
-                fgets(searchText, sizeof(searchText), stdin);
-                size_t textLength = strlen(searchText);
-                if (textLength > 0 && searchText[textLength - 1] == '\n') {
-                    searchText[textLength - 1] = '\0';
-                }
-
-                TextNode *currentNode = listHead;
-                bool isFound = false;
-                int lineNumber = 0;
-                while (currentNode) {
-                    if (strstr(currentNode->content, searchText)) {
-                        printf("Found on line %d: %s\n", lineNumber, currentNode->content);
-                        isFound = true;
-                    }
-                    lineNumber++;
-                    currentNode = currentNode->next;
-                }
-                if (!isFound) {
-                    printf("Text not found!\n");
-                }
+                searchTextInList(listHead, bufferLength);
                 break;
-            }
             case '8':
-                displayMenuAndClearConsole();
+                clearConsole();
+                displayMenu();
                 break;
             case '9':
                 printf("Exiting program...\n");
@@ -244,4 +278,3 @@ int main() {
     }
     return 0;
 }
-
